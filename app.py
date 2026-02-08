91c0dc7bdb86860f55cb46ad7c61c1192586c56c
Jamie Bryson <jamieab98@gmail.com>
2026-02-08 13:17:19 -0500

    Finished the basics without verifications

--- app.py
+++ app.py
@@ -21,33 +21,29 @@ events = [
 # Create a new event from JSON input
 @app.route("/events", methods=["POST"])
 def create_event():
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
+    data = request.get_json()
+    new_id = max([e.id for e in events], default = 0) + 1
+    new_event = Event(new_id, data["title"])
+    events.append(new_event)
+    return jsonify(new_event.to_dict()), 201
     pass
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
+    data = request.get_json()
+    event = next((e for e in events if e.id == event_id), None)
+    event.title = data["title"]
+    return jsonify(event.to_dict()), 200
     pass
 
 # TODO: Task 1 - Define the Problem
 # Remove an event from the list
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
+    global events
+    events = [e for e in events if e.id != event_id]
     pass
 
 if __name__ == "__main__":
